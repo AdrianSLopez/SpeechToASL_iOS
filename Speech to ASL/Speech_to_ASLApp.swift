7c66a1d5d8097b9a9e63b3d7cbe2987383af1892
AdrianSLopez <adrianlopezdev@gmail.com>
2024-01-30 12:27:39 -0800

    Initial Commit

---
+++ Speech to ASL/Speech_to_ASLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speech_to_ASLApp.swift
+//  Speech to ASL
+//
+//  Created by Adrian Salgado Lopez on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Speech_to_ASLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Speech to ASLUITests/Speech_to_ASLUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Speech_to_ASLUITestsLaunchTests.swift
+//  Speech to ASLUITests
+//
+//  Created by Adrian Salgado Lopez on 1/30/24.
+//
+
+import XCTest
+
+final class Speech_to_ASLUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
